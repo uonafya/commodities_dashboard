d193cd3e024470a39af2ecc7827fb10005dbf640
Benzer Bett <benzerbett@gmail.com>
2018-11-09 10:11:44 +0300

    national nav, moved some rrDetails Fxns to separate JS file

--- dist/js/rrdetails.js
+++ dist/js/rrdetails.js
@@ -1,65 +1,104 @@
-var rrdettable = $('#rrdettable');
-var rrdettable_body = $('#rrdettable tbody');
-var rdurl = 'http://localhost/hcdlive/commodities_dashboard/html/ltr/reportingdetails.json'
-$.ajax({
-    type: 'GET',
-    crossDomain: true,
-    url: rdurl,                    
-    success: function (rdData) {
-        $('.loader-sp').addClass('hidden');
-        rrdettable.removeClass('hidden');
-        plotRRDetable(rdData);
-    },
-    error: function (request, status, error) {
-        $('.loader-sp').addClass('hidden');
-        rrdettable.addClass('hidden');
-        console.log('RRates: error fetching json. :- '+error);
-        $('.state.col-md-12').html('<div class ="alert alert-danger"><strong>Error</strong><br/>Failed to load this table. Please <a href="#" class="btn btn-xs btn-primary btn-rounded" onclick="window.location.reload(true)">refresh</a> this page to retry</div>');
-    }
-});
+//function toprocess the acts url
+function fetchRRDetails(rdurl)
+{
+    $.getJSON(rdurl, function (data) 
+    {                    
+            var header = '';
+            var footer = '';
+            var tableData = '';
+            
+            //put the header
+            header += '<thead><tr>';	
+            header += '<th>Name</th>';
 
-function plotRRDetable(rdData){
-    period = rdData.metaData.dimensions.pe;
-    rdRows = rdData.rows;
-    rdItems = rdData.metaData.items;
-    ous = rdData.metaData.dimensions.ou;
-    var tablehead = '<td>Sub-county/Facility</td>';
-    var trows = '';
-    $.each(period, function(index, eachperiod){
-        tablehead+=('<td>'+eachperiod+'</td>');
-    });
-    var rowdata = [];
-    $.each(ous, function(indexy, eachou){
-        $.each(rdRows, function(index, eachrow){
-            if(eachrow.includes(eachou)){
-                trows+='<tr>';
-                    trows+='<td>'+eachrow[2]+'</td>';
-                    $.each(period, function(indexz, eachnesperiod){
-                        var theval = checkPeriod(eachrow,eachnesperiod);
-                        if(theval == 1){
-                            trows+='<td>'+theval+'</td>';
-                        }else{
-                            trows+='<td style="background: #ffc7ce;"></td>';
+            $.each(data.metaData.dimensions.pe, function (pkey, pentry) 
+            {
+                    header += '<th>'+pentry+'</th>';			
+            })
+
+            header += '</tr></thead>';
+
+            //put the footer
+            footer += '<tfoot><tr>';	
+            footer += '<th>Name</th>';
+
+            $.each(data.metaData.dimensions.pe, function (pkey, pentry) 
+            {
+                    footer += '<th>'+pentry+'</th>';			
+            })
+
+            footer += '</tr></tfoot>';
+
+            tableData += header;
+
+            //start body
+            tableData += '<tbody>';
+
+            //console.log(orgunits.length);
+            $.each(data.metaData.dimensions.ou, function (key, entry) 
+            {                                                        
+                tableData += '<tr>';	
+                tableData += '<td>'+data.metaData.items[entry].name+'</td>';
+
+                $.each(data.metaData.dimensions.pe, function (pkey, pentry) 
+                {
+                        var reportval = getReport(data.rows,pentry,entry);
+                        if(reportval)
+                        {
+                            tableData += '<td>'+reportval+'</td>';	
                         }
-                    });
-                trows+='</tr>';
-            }
-        });
-    });
-    rrdettable_body.html(trows);
-    $('#rrdettable thead tr').html(tablehead);
-    $('#rrdettable tfoot tr').html(tablehead);
-    rrdettable.DataTable();
-    
-    
-    
-    function checkPeriod(onerow,perd){
-        if(onerow.includes(perd)){
-            return onerow[3];
-        }else{
-            return '';
-        }
-    }
+                        else
+                        {
+                            var bgcolor = '#ffc7ce';
+                            tableData += '<td bgcolor="'+bgcolor+'">'+reportval+'</td>';
+                        }
+                })
 
-    // console.log('Hiii! :->  '+JSON.stringify(rdRows));
+                tableData += '</tr>';	
+            })
+
+            //footer line
+            tableData += '</tbody>';
+            tableData += footer;
+                                
+            //$('#zero_config').DataTable().destroy();
+            $("table.rrdetailsbox").empty();
+            $("table.rrdetailsbox").append(tableData);	
+            $('#zero_config').DataTable();                                                          
+            $('#zero_config').DataTable().draw();
+    })
+}
+
+
+//get the org unit
+function getOrgUnit(uid, period)
+{
+var url = 'https://testhis.uonbi.ac.ke/dhis/api/organisationUnits/'+uid+'.json?fields=id,name';
+
+$.ajax({      
+  dataType: "json",
+  url: url, //Relative or absolute path to response.php file      
+  success: function(data) 
+  {          
+      $("h5.card-title").html(data['name']+' - '+period);
+  }
+});    
+}
+
+//get the report details
+function getReport(rows,period,orgunit) 
+{	
+var rowval = '';
+
+//loop through the rows
+$.each(rows, function (rkey, rentry) 
+{
+        //check for orgunit and period
+        if(orgunit==rentry[2] && period==rentry[1])
+        {                                    
+                rowval = parseInt(rentry[3]);
+        }								
+})		
+
+return rowval;
 }
