54cef6d3334d8f5aac375c358eeacddd26fd3b51
Benzer Bett <benzerbett@gmail.com>
2018-11-16 10:47:48 +0300

    rrdetails + endpoint

--- dist/js/rrdetails.js
+++ dist/js/rrdetails.js
@@ -64,22 +64,114 @@ function fetchRRDetails(rdurl)
             tableData += '</tbody>';
             tableData += footer;
                     
+            //facility
             $('#zero_config').removeClass('hidden');
-            //$('#zero_config').DataTable().destroy();
-            $("table.rrdetailsbox").empty();
+            $("table.rrdetailsbox tbody").empty();
             $("table.rrdetailsbox").append(tableData);	
             $('#zero_config').DataTable();                                                          
             $('#zero_config').DataTable().draw();
             $('.loader-sp').addClass('hidden');
-
+            
     },
     error: function (request, status, error) {
         $('.loader-sp').addClass('hidden');
         console.log('Reporting Rate Details: error fetching json. :- '+error);
-        $('.rdstate').html('<div class ="alert alert-danger"><strong>Data Error</strong><br/>Failed to load this data. Please <a href="#" class="btn btn-xs btn-primary btn-rounded" onclick="window.location.reload(true)">refresh</a> this page to retry</div>');
+        $('.rdstate').html('<div class ="alert alert-danger"><strong>Facility Data Error</strong><br/>Failed to load this data. Please <a href="#" class="btn btn-xs btn-primary btn-rounded" onclick="window.location.reload(true)">refresh</a> this page to retry</div>');
     }
 });
 }
+
+
+
+
+
+
+function fetchSubRRDetails(scrdurl)
+{
+    $.ajax({
+        type: 'GET',
+        crossDomain: true,
+        url: scrdurl,                    
+        success: function (data) {                    
+            var header = '';
+            var footer = '';
+            var tableData = '';
+            
+            //put the header
+            header += '<thead><tr>';	
+            header += '<th>Name</th>';
+
+            $.each(data.metaData.dimensions.pe, function (pkey, pentry) 
+            {
+                    header += '<th>'+pentry+'</th>';			
+            })
+
+            header += '</tr></thead>';
+
+            //put the footer
+            footer += '<tfoot><tr>';	
+            footer += '<th>Name</th>';
+
+            $.each(data.metaData.dimensions.pe, function (pkey, pentry) 
+            {
+                    footer += '<th>'+pentry+'</th>';			
+            })
+
+            footer += '</tr></tfoot>';
+
+            tableData += header;
+
+            //start body
+            tableData += '<tbody>';
+
+            //console.log(orgunits.length);
+            $.each(data.metaData.dimensions.ou, function (key, entry) 
+            {                                                        
+                tableData += '<tr>';	
+                tableData += '<td>'+data.metaData.items[entry].name+'</td>';
+
+                $.each(data.metaData.dimensions.pe, function (pkey, pentry) 
+                {
+                        var reportval = getReport(data.rows,pentry,entry);
+                        if(reportval)
+                        {
+                            tableData += '<td>'+reportval+'</td>';	
+                        }
+                        else
+                        {
+                            var bgcolor = '#ffc7ce';
+                            tableData += '<td bgcolor="'+bgcolor+'">'+reportval+'</td>';
+                        }
+                })
+
+                tableData += '</tr>';	
+            })
+
+            //footer line
+            tableData += '</tbody>';
+            tableData += footer;
+                    
+            //subcounty
+            $('#zero_config-sub').removeClass('hidden');
+            $("table.rrdetailsbox-sub tbody").empty();
+            $("table.rrdetailsbox-sub").append(tableData);	
+            $('#zero_config-sub').DataTable();                                                          
+            $('#zero_config-sub').DataTable().draw();
+            $('.loader-sp.sp-sub').addClass('hidden');
+
+    },
+    error: function (request, status, error) {
+        $('.loader-sp.sp-sub').addClass('hidden');
+        console.log('Reporting Rate Details: error fetching json. :- '+error);
+        $('.rdstate-sub').html('<div class ="alert alert-danger"><strong>Sub-county Data Error</strong><br/>Failed to load this data. Please <a href="#" class="btn btn-xs btn-primary btn-rounded" onclick="window.location.reload(true)">refresh</a> this page to retry</div>');
+    }
+});
+}
+
+
+
+
+
 
 
 //get the org unit
