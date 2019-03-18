a5ac58f0b63a8d4232f86589ce3fe22354765aee
Benzer Bett <benzerbett@gmail.com>
2019-03-18 16:29:39 +0300

    cell formatting

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -173,4 +173,18 @@ $.fn.digits = function(){
     })
 }
 // 000s number formatting fxn
+
+
+function numberWithCommas(number) {
+    var parts = number.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
+}
+$(document).ready(function() {
+  $(".table td").each(function() {
+    var num = $(this).text();
+    var commaNum = numberWithCommas(num);
+    $(this).text(commaNum);
+  });
+});
 //---------------------------end custom---------------------------//
