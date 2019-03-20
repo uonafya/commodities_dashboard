5a59161a6a6156c1298af477b0d5829da1c44994
Benzer Bett <benzerbett@gmail.com>
2019-03-20 10:01:46 +0300

    IS-trends

--- dist/js/indicatorsummary-trends.js
+++ dist/js/indicatorsummary-trends.js
@@ -8,24 +8,66 @@ function getTrends(the_url) {
             $('.loader-sp.is-sub').addClass('hidden');
             $('#indi-trends').removeClass('hidden');
             
+            // var thedata = [];
+            // var the_periods = [];
+            // $.each(data.metaData.dimensions.dx, function (indx, one_dx) {
+            //     var data_foreach_commo = {};
+            //     data_foreach_commo['name'] = data.metaData.items[one_dx].name;
+            //     data_foreach_commo['data'] = [];
+            //     $.each(data.metaData.dimensions.pe, function (indx2, one_pe) {
+            //         the_periods.push(data.metaData.items[one_pe].name);
+            //         var datapoint = 0;
+            //         $.each(data.rows, function (indx3, one_row) { 
+            //             if(one_row[0] == one_dx){
+            //                 datapoint = datapoint + 1;
+            //             }
+            //         });
+            //         data_foreach_commo['data'].push(datapoint);
+            //     });
+            //     thedata.push(data_foreach_commo);
+            // });
+
+
+            var commo_name = '';
             var thedata = [];
             var the_periods = [];
             $.each(data.metaData.dimensions.dx, function (indx, one_dx) {
-                var data_foreach_commo = {};
-                data_foreach_commo['name'] = data.metaData.items[one_dx].name;
-                data_foreach_commo['data'] = [];
+                commo_name = data.metaData.items[one_dx].name;
+                var overstock_arr = {};
+                overstock_arr['name'] = 'Overstocked';
+                overstock_arr['data'] = [];
+                var stockok_arr = {};
+                stockok_arr['name'] = 'Stocked according to plan';
+                stockok_arr['data'] = [];
+                var understock_arr = {};
+                understock_arr['name'] = 'Understocked';
+                understock_arr['data'] = [];
                 $.each(data.metaData.dimensions.pe, function (indx2, one_pe) {
+                    var overstock = 0;
+                    var stockok = 0;
+                    var understock = 0;
                     the_periods.push(data.metaData.items[one_pe].name);
-                    var datapoint = 0;
-                    $.each(data.rows, function (indx3, one_row) { 
-                        // console.log('dx:'+one_dx+' == rw: '+one_row[0]);
+                    $.each(data.rows, function (indx3, one_row) {
+                        var row_val = one_row[3];
                         if(one_row[0] == one_dx){
-                            datapoint = datapoint + 1;
+                            if(row_val>6){
+                                overstock++;
+                            }
+                            if(row_val>=3 && row_val<=6){
+                                stockok++;
+                            }
+                            if(row_val>0 && row_val<3){
+                                understock++;
+                            }
                         }
                     });
-                    data_foreach_commo['data'].push(datapoint);
+                    overstock_arr['data'].push(overstock);
+                    stockok_arr['data'].push(stockok);
+                    understock_arr['data'].push(understock);
                 });
-                thedata.push(data_foreach_commo);
+                thedata.push(overstock_arr);
+                thedata.push(stockok_arr);
+                thedata.push(understock_arr);
             });
                 
     
@@ -38,8 +80,7 @@ function getTrends(the_url) {
                     text: 'Stock Status Trend For The Last 6 Months'
                 },
                 subtitle: {
-                    // text: 'Source: Dhis2'
-                    text: ' '
+                    text: 'Commodity: '+commo_name
                 },
                 xAxis: {
                     categories: the_periods
