c40825bcce72b7123270b95da4a8ee6ee414d8c3
Benzer Bett <benzerbett@gmail.com>
2019-03-21 14:16:08 +0300

    accountability math check

--- dist/js/acc-v2.js
+++ dist/js/acc-v2.js
@@ -102,7 +102,7 @@ function fetchAccountability(url,orgun) {
                     var sum_neg = qtydisp[3]+negadj[3]+closbal[3];
                     var per_acc_for = parseFloat(sum_neg)/parseFloat(sum_pos);
                     console.log("per_acc_for: "+per_acc_for);
-                    tabl+='<td class="text-right">'+per_acc_for.toFixed(1)+'%</td>';
+                    tabl+='<td class="text-right">'+(per_acc_for*100).toFixed(1)+'%</td>';
                     pcacc_arr.push(per_acc_for);
 
                     // ----------END data cells----------
