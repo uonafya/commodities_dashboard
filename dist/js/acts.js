8fb86ab6c026c8a8fc9a6e2a92d1626224e11f93
Benzer Bett <benzerbett@gmail.com>
2019-03-26 14:32:07 +0300

    renaming ACTs

--- dist/js/acts.js
+++ dist/js/acts.js
@@ -161,7 +161,7 @@ function fetchACTs(acturl,orgu,peri)
                 }
             })
 
-            $('#thetitle').html('ACTs <br/> in '+facility_count+' facilities');
+            $('#thetitle').html('AL <br/> in '+facility_count+' facilities');
             //tableData += '<table>';	
             //console.log(tableData)
             $('#acts_table').removeClass('hidden');
