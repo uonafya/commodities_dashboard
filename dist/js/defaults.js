bcf1e7e29468ad43c00735841d0bfa727a5aff58
Benzer Bett <benzerbett@gmail.com>
2019-04-09 10:27:42 +0300

    defaults sctipt: OU (WIP)

--- dist/js/defaults.js
+++ dist/js/defaults.js
@@ -1,14 +1,34 @@
-function myOU(getOU) {
+// function myOU(getOU) {
+//     $.ajax({
+//         type: "GET",
+//         crossOrigin: true,
+//         url:"https://testhis.uonbi.ac.ke/api/me.json",  
+//         success:function(response) {
+//             var ous = response.organisationUnits;
+//             var dataViewOU = response.dataViewOrganisationUnits[0];
+//             console.log('dataViewOU ==> '+JSON.stringify(dataViewOU));
+//             console.log('OUs ==> '+JSON.stringify(ous));
+//             getOU(data); 
+//         }
+//     });
+//   }
+
+
+function myOU() {
+    var dataViewOU;
+    var ous;
     $.ajax({
-        type: "GET",
-        crossOrigin: true,
-        url:"https://testhis.uonbi.ac.ke/api/me.json",  
-        success:function(response) {
-            var ous = response.organisationUnits;
-            var dataViewOU = response.dataViewOrganisationUnits[0];
+        url:'https://testhis.uonbi.ac.ke/api/me.json',
+        data: data,
+        async: false,
+        success: function (resp) {
+            ous = response.organisationUnits;
+            dataViewOU = resp.dataViewOrganisationUnits[0];
             console.log('dataViewOU ==> '+JSON.stringify(dataViewOU));
             console.log('OUs ==> '+JSON.stringify(ous));
-            getOU(data); 
-        }
+            callback.call(dataViewOU);
+        },
+        error: function () {}
     });
-  }
+    return dataViewOU;
+}
