95a350966591be18edf8865c9de8cd93888cffd8
Benzer Bett <benzerbett@gmail.com>
2019-04-09 10:12:13 +0300

    defaults sctipt: OU (wIp)

--- dist/js/defaults.js
+++ dist/js/defaults.js
@@ -1,17 +1,18 @@
 function myOU() {
     var end_url = 'https://testhis.uonbi.ac.ke/api/me.json';
-    var end_url = 'http://localhost/pmi/json/me-abdi.json';
+    // var end_url = 'http://localhost/pmi/json/me-abdi.json';
 
-    $.ajax({
+    return $.ajax({
         type: "GET",
         url: "end_url",
         crossOrigin: true,
         success: function (response) {
             var ous = response.organisationUnits;
             var dataViewOU = response.dataViewOrganisationUnits[0];
-            alert('response.dataViewOrganisationUnits[0] ==>'+response.dataViewOrganisationUnits[0])
+            // alert('response.dataViewOrganisationUnits[0] ==>'+response.dataViewOrganisationUnits[0])
             log('OUs ==> '+JSON.stringify(ous));
             log('dataViewOU ==> '+JSON.stringify(dataViewOU));
+            return dataViewOU;
         }
     });
 }
