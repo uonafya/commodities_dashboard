7b850fdf5159d99e4f0d980b1202cfa017cd7fd6
Benzer Bett <benzerbett@gmail.com>
2019-04-09 10:34:19 +0300

    defaults sctipt: OU (WIP)

--- dist/js/defaults.js
+++ dist/js/defaults.js
@@ -1,18 +1,20 @@
-// function myOU(getOU) {
+// function myOU() {
+//     var dataViewOU;
+//     var ous;
 //     $.ajax({
-//         type: "GET",
-//         crossOrigin: true,
-//         url:"https://testhis.uonbi.ac.ke/api/me.json",  
-//         success:function(response) {
-//             var ous = response.organisationUnits;
-//             var dataViewOU = response.dataViewOrganisationUnits[0];
+//         url:'https://testhis.uonbi.ac.ke/api/me.json',
+//         async: false,
+//         success: function (resp) {
+//             ous = resp.organisationUnits;
+//             dataViewOU = resp.dataViewOrganisationUnits[0];
 //             console.log('dataViewOU ==> '+JSON.stringify(dataViewOU));
 //             console.log('OUs ==> '+JSON.stringify(ous));
-//             getOU(data); 
-//         }
+//             callback.call(dataViewOU);
+//         },
+//         error: function () {}
 //     });
-//   }
-
+//     return dataViewOU;
+// }
 
 function myOU() {
     var dataViewOU;
@@ -20,12 +22,12 @@ function myOU() {
     $.ajax({
         url:'https://testhis.uonbi.ac.ke/api/me.json',
         async: false,
+        data: 'my OUs',
         success: function (resp) {
             ous = resp.organisationUnits;
             dataViewOU = resp.dataViewOrganisationUnits[0];
             console.log('dataViewOU ==> '+JSON.stringify(dataViewOU));
             console.log('OUs ==> '+JSON.stringify(ous));
-            callback.call(dataViewOU);
         },
         error: function () {}
     });
