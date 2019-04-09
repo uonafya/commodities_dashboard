e48d0df9854cd59387c19aeed0cd357430a86d84
Benzer Bett <benzerbett@gmail.com>
2019-04-09 11:03:27 +0300

    defaults sctipt: OU (WIP) II

--- dist/js/defaults.js
+++ dist/js/defaults.js
@@ -19,6 +19,9 @@
 function myOU() {
     var dataViewOU;
     var ous;
+    var my_data_ou;
+    var my_county;
+    var my_subcounty;
     $.ajax({
         url:'https://testhis.uonbi.ac.ke/api/me.json',
         async: false,
@@ -26,10 +29,24 @@ function myOU() {
         success: function (resp) {
             ous = resp.organisationUnits;
             dataViewOU = resp.dataViewOrganisationUnits[0];
-            console.log('dataViewOU ==> '+JSON.stringify(dataViewOU));
-            console.log('OUs ==> '+JSON.stringify(ous));
+            my_data_ou = dataViewOU["id"];
+            var level;
+            $.ajax({
+                type: "GET",
+                url: "https://testhis.uonbi.ac.ke/api/organisationUnits/"+my_data_ou+".json?fields=ancestors,level",
+                async: false,
+                data: "my OUs2",
+                success: function (response) {
+                    level = response.level;
+                    if(level > 1){
+                        my_county = response.ancestors[1];
+                    }else{
+                        my_county = my_data_ou;
+                    }
+                }
+            });
         },
         error: function () {}
     });
-    return dataViewOU["id"];
+    return my_county;
 }
