3e768e59a108cb5e7a7a3eaef433cc8882de8e28
Benzer Bett <benzerbett@gmail.com>
2019-04-10 15:25:45 +0300

    fixing DQ titles

--- dist/js/defaults.js
+++ dist/js/defaults.js
@@ -21,20 +21,15 @@ function myOU() {
                 success: function (response) {
                     level = response.level;
                     if(level > 2){
-                        console.log('hoooo: '+JSON.stringify(response.ancestors));
                         if(response.ancestors[1]["id"] !== undefined){
                             my_county = response.ancestors[1]["id"];
-                            console.log('oooone');
                         }else{
                             my_county = response.ancestors[0]["id"];
-                            console.log('twoooo');
                             
                         }
-                        console.log('my_county (level > 1): '+my_county);
                         $('#county-dropdown option[value='+my_county+']').attr('selected', 'selected');
                     }else{
                         my_county = my_data_ou;
-                        console.log('my_county (else): '+my_county)
                     }
 
                     if(my_county == 'HfVjCurKxh2'){
