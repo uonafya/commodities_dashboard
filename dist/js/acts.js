de2cdf706a7486684ce1eed37f8d0683c1c4b48f
Benzer Bett <benzerbett@gmail.com>
2019-04-11 11:52:02 +0300

    fix ACTs title

--- dist/js/acts.js
+++ dist/js/acts.js
@@ -179,7 +179,7 @@ function fetchACTs(acturl,orgu,peri)
                 }
             });
             // title fill
-                var url = 'https://testhis.uonbi.ac.ke/api/organisationUnits/'+myOU()+'.json?fields=id,name';
+                var url = 'https://testhis.uonbi.ac.ke/api/organisationUnits/'+orgu+'.json?fields=id,name';
                 $.ajax({      
                     dataType: "json",
                     url: url,
