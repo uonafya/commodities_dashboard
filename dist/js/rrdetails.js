03efd437e10e629edd4c4db17ebed29be92614b1
Benzer Bett <benzerbett@gmail.com>
2019-08-08 16:48:41 +0300

    county summary row for reportingrate IV.4.47

--- dist/js/rrdetails.js
+++ dist/js/rrdetails.js
@@ -8,45 +8,8 @@ function fetchRRDetails(theperiod,ounit)
     $('.loader-sp').removeClass('hidden');
     var summary_row = ''
 
-    $(document).ready(function () {
-        $.ajax({
-            type: "GET",
-            url: 'https://testhis.uonbi.ac.ke/api/26/analytics.json?dimension=dx:JPaviRmSsJW.ACTUAL_REPORTS;JPaviRmSsJW.EXPECTED_REPORTS&dimension=pe:'+theperiod+'&dimension=ou:'+ounit+';LEVEL-2&displayProperty=NAME&outputIdScheme=UID',
-            data: "data",
-            success: function (resp) {
-                $.each(resp.metaData.dimensions.ou, function (key, entry){                                                        
-                    console.log("SUCCESSFULLY FETCHING COUNTY SUMMARY FOR: "+resp.metaData.items[entry].name);
-                    summary_row += '<tr style="font-size: 1.16em;">';	
-                    summary_row += '<td> SUMMARY: '+resp.metaData.items[entry].name+'</td>';
-                    $.each(resp.metaData.dimensions.pe, function (pkey, pentry) 
-                    {
-                            var rpt_count = getExpectedSub(resp.rows,pentry,entry);
-                            var reportval = getReport(resp.rows,pentry,entry);
-                            if(reportval)
-                            {
-                                if(reportval==rpt_count)
-                                {
-                                    summary_row += '<td style="background-color: #77ff77;" class="text-bold">'+reportval+'/'+rpt_count+'</td>';
-                                }
-                                else
-                                {
-                                    summary_row += '<td style="background-color: #ffeb9c;" class="text-bold">'+reportval+'/'+rpt_count+'</td>';
-                                }
-                            }
-                            else
-                            {
-                                var bgcolor = '#ffeb9c';
-                                summary_row += '<td style="border: 1px solid #fff;" bgcolor="'+bgcolor+'" class="text-bold">'+reportval+'/'+rpt_count+'</td>';
-                            }
-                    })
-                    summary_row += '</tr>';	
-                })
-                console.log('COUNTY SUMMARY ROW='+summary_row);
-            }
-        });
-    });
     
-    sleep(3500);
+    
     $.ajax({
         type: 'GET',
         crossDomain: true,
@@ -111,8 +74,6 @@ function fetchRRDetails(theperiod,ounit)
                 tableData += '</tr>';	
             })
 
-            tableData += summary_row;
-
             tableData += '</tbody>';
             //footer line
             // tableData += footer;
@@ -160,6 +121,46 @@ function fetchRRDetails(theperiod,ounit)
             // $('.rdstate').html('<div class ="alert alert-danger"><strong>'+request.responseJSON.httpStatusCode+': '+request.responseJSON.message+'</strong><br/>Failed to load this data. Please <a href="#" class="btn btn-xs btn-primary btn-rounded" onclick="window.location.reload(true)">refresh</a> this page to retry</div>');
             $('.rdstate').html('<div class ="alert alert-danger"><strong>'+status+'</strong><br/>Failed to load this data. Please <a href="#" class="btn btn-xs btn-primary btn-rounded" onclick="window.location.reload(true)">refresh</a> this page to retry</div>');
         }
+    });
+
+    sleep(2000);
+    $(document).ready(function () {
+        $.ajax({
+            type: "GET",
+            url: 'https://testhis.uonbi.ac.ke/api/26/analytics.json?dimension=dx:JPaviRmSsJW.ACTUAL_REPORTS;JPaviRmSsJW.EXPECTED_REPORTS&dimension=pe:'+theperiod+'&dimension=ou:'+ounit+';LEVEL-2&displayProperty=NAME&outputIdScheme=UID',
+            data: "data",
+            success: function (resp) {
+                $.each(resp.metaData.dimensions.ou, function (key, entry){                                                        
+                    console.log("SUCCESSFULLY FETCHING COUNTY SUMMARY FOR: "+resp.metaData.items[entry].name);
+                    summary_row += '<tr style="font-size: 1.16em;">';	
+                    summary_row += '<td> SUMMARY: '+resp.metaData.items[entry].name+'</td>';
+                    $.each(resp.metaData.dimensions.pe, function (pkey, pentry) 
+                    {
+                            var rpt_count = getExpectedSub(resp.rows,pentry,entry);
+                            var reportval = getReport(resp.rows,pentry,entry);
+                            if(reportval)
+                            {
+                                if(reportval==rpt_count)
+                                {
+                                    summary_row += '<td style="background-color: #77ff77;" class="text-bold">'+reportval+'/'+rpt_count+'</td>';
+                                }
+                                else
+                                {
+                                    summary_row += '<td style="background-color: #ffeb9c;" class="text-bold">'+reportval+'/'+rpt_count+'</td>';
+                                }
+                            }
+                            else
+                            {
+                                var bgcolor = '#ffeb9c';
+                                summary_row += '<td style="border: 1px solid #fff;" bgcolor="'+bgcolor+'" class="text-bold">'+reportval+'/'+rpt_count+'</td>';
+                            }
+                    })
+                    summary_row += '</tr>';	
+                })
+                console.log('COUNTY SUMMARY ROW='+summary_row);
+                $('#facility_rr tbody').append()
+            }
+        });
     });
     
 }
